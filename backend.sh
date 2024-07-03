0bed120bcf16396ba9e80f7f5abb699bb3879e1c
nagasai <avuthunagasaireddy@gmail.com>
2024-07-03 16:42:48 +0530

    learning devops

--- backend.sh
+++ backend.sh
@@ -45,8 +45,34 @@ else
     echo -e "Expense user already created...$Y SKIPPING $N"
 fi
 
+mkdir -p /app &>>LOGFILE
+VALIDATE $? "Creating app directory"
 
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>LOGFILE
+VALIDATE $? "Downloading the code"
 
+cd /app
 
+unzip /tmp/backend.zip
+VALIDATE $? "unzipping the content"
 
-mkdir -p /app
+cd /app &>>LOGFILE
+
+npm install &>>LOGFILE
+
+cp /home/ec2-user/expenses-shell/backend.service  /etc/systemd/system/backend.service
+Validate $? "SystemD Expense Backend Service"
+
+systemctl daemon-reload &>>LOGFILE
+VALIDATE $? "daemon-reload"
+systemctl start backend &>>LOGFILE
+VALIDATE $? "starting backend"
+systemctl enable backend &>>LOGFILE
+VALIDATE $? "enabling backend"
+
+mysql -h db-dev.devopsb78.tech -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>LOGFILE
+VALIDATE $? "loading schema"
+
+systemctl restart backend &>>LOGFILE
+VALIDATE $? "Restarting backend"
+
