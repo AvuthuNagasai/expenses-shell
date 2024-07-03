7deb1619ef8e250a1bf16a219a87d7acf915e15c
nagasai <avuthunagasaireddy@gmail.com>
2024-07-03 15:50:33 +0530

    learning devops

---
+++ db-shell.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME$TIMESTAMP.log
+
+VALIDATE(){
+   if [ $1 -ne 0 ]
+   then
+        echo "$2...FAILURE"
+        exit 1
+    else
+        echo -e " $R $2...SUCCESS"
+    fi
+}
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+C="\e[36m"
+
+
+
+echo "Please enter DB password:"
+read -s mysql_root_password
+
+if [ $USERID -ne 0 ]
+then
+  echo -e "$R you must run the code as a super user"
+  exit 1
+else
+  echo -e "$Y you are a super user"
+fi
+
+dnf install mysql-server -y $>>LOGFILE
+VALIDATE $? "installing my sql server"
+
+systemctl enable mysqld $>>LOGFILE
+VALIDATE $? "enabling my sql server"
+
+systemctl start mysqld $>>LOGFILE
+VALIDATE $? "starting my sql server"
+
+
+#mysql_secure_installation --set-root-pass ExpenseApp@1
+#VALIDATE $? "Setting up root password"
+
+mysql_secure_installation --set-root-pass $(mysql_root_password) &>>LOGFILE
+
+if [ $? -ne 0 ]
+then
+  mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
+else
+  echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
+fi
