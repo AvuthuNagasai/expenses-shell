6c2926f645c40b27f5dfa693c35daa864095d4bc
nagasai <avuthunagasaireddy@gmail.com>
2024-07-03 22:00:19 +0530

    learning devops

--- backend.sh
+++ backend.sh
@@ -160,8 +160,8 @@ VALIDATE $? "Enabling backend"
 dnf install mysql -y &>>$LOGFILE
 VALIDATE $? "Installing MySQL Client"
 
-mysql -h db-dev.devopsb73.tech -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
-#mysql -h 172.31.191.191 -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
+#mysql -h db-dev.devopsb73.tech -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
+mysql -h 172.31.84.205 -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
 VALIDATE $? "Schema loading"
 
 systemctl restart backend &>>$LOGFILE
